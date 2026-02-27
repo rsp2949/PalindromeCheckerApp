2bd1d6cceadc54d194547d3c037250ecc80ebb7c
rsp2949 <praveenrsp10@gmail.com>
2026-02-27 15:17:28 +0530

    The app is initialized

---
+++ App/src/Palindrome.java
@@ -0,0 +1,7 @@
+public class Palindrome {
+    static void main() {
+        System.out.println("welcome to palindrome app");
+        System.out.println("version 1.0");
+        System.out.println("System initiallized successfully");
+    }
+}
