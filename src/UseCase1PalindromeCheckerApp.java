24ec353b0f32388a180ced0334a8574b424037da
rsp2949 <praveenrsp10@gmail.com>
2026-02-27 15:54:38 +0530

    UC1: Added welcome message implementation

---
+++ src/UseCase1PalindromeCheckerApp.java
@@ -0,0 +1,10 @@
+public class UseCase1PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Palindrome Checker App");
+        System.out.println("Version: 1.0");
+        System.out.println("System initialized successfully");
+
+    }
+}
